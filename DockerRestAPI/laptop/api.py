cd63a9fe14980a1b4bd6a6c4ac5a7f3b3b07dca9
quinnmil <quinnmil@gmail.com>
2018-03-02 17:43:03 -0800

    Create API that outputs brevet entries using JSON. Add DockerMongo to repo and edit docker-compose to work with all resources.

--- DockerRestAPI/laptop/api.py
+++ DockerRestAPI/laptop/api.py
@@ -1,11 +1,22 @@
 # Laptop Service
-
-from flask import Flask
+import flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+import pymongo
+from pymongo import MongoClient
+import sys
+import os
+import logging
 
 # Instantiate the app
-app = Flask(__name__)
+app = flask.Flask(__name__)
 api = Api(app)
+
+
+client = MongoClient('db', 27017)
+db = client.tododb
+collection = db.control
+
 
 class Laptop(Resource):
     def get(self):
@@ -17,10 +28,56 @@ class Laptop(Resource):
             ]
         }
 
+class HelloWorld (Resource):
+	def get(self):
+		return {'value': 'hello world'}
+
+class ListAll (Resource):
+    def get(self):
+        allTimes = collection.find()
+        # app.logger.debug(entries[0])
+        result = []
+        for entry in allTimes:
+            result.append({
+                'open' : entry ['open_time'],
+                'close': entry ['close_time'],
+                'km' : entry ['km']
+                })
+
+        return flask.jsonify(result= result)
+
+class ListOpenOnly (Resource):
+    def get(self):
+        allOpen = collection.find()
+        result = []
+        for entry in allOpen:
+            result.append({
+                'open' : entry ['open_time']
+                })
+        return flask.jsonify(result = result)
+
+
+
+class ListClosedOnly (Resource):
+       def get(self):
+        allClose = collection.find()
+        result = []
+        for entry in allClose:
+            result.append({
+                'close' : entry ['close_time']
+                })
+        return flask.jsonify(result = result)
+
+
+
 # Create routes
 # Another way, without decorators
 api.add_resource(Laptop, '/')
+api.add_resource(HelloWorld,'/hi')
 
+api.add_resource(ListAll,'/listAll','/listAll/json')
+api.add_resource(ListOpenOnly, '/ListOpenOnly', '/ListOpenOnly/json')
+api.add_resource(ListClosedOnly, '/ListClosedOnly','/ListCLosedOnly/json')
 # Run the application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
--- DockerRestAPI/website/index.php
+++ DockerRestAPI/website/index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>CIS 322 REST-api demo: Laptop list</title>
+        <title>Project 6 - restful api</title>
     </head>
 
     <body>
@@ -9,11 +9,89 @@
             <?php
             $json = file_get_contents('http://laptop-service/');
             $obj = json_decode($json);
-	          $laptops = $obj->Laptops;
+	           $laptops = $obj->Laptops;
             foreach ($laptops as $l) {
                 echo "<li>$l</li>";
             }
             ?>
         </ul>
+        <h1> Hello:</h1>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/hi');
+            $obj = json_decode($json);
+                $phrase = $obj->value;
+                echo "<li> $phrase </li>"; 
+            ?>
+        </ul>
+
+        <h1>/listAll</h1>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listAll');
+
+            $obj = json_decode($json);
+            $times = $obj->result;
+            if ($times == []){
+                echo "Database is empty"; 
+            } else{
+                foreach($times as $k) {
+                    $open = $k->open;
+                    $close = $k->close;
+                    $distance = $k->km;
+                    echo "<li>Open:$open</li><li>Close:$close</li><li>Distance:$distance</li>";
+
+                }
+
+            }
+            ?>
+        </ul>
+        <h1>/ListOpenOnly</h1>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/ListOpenOnly');
+
+            $obj = json_decode($json);
+            $times = $obj->result;
+            if ($times == []){
+                echo "Database is empty"; 
+            } else{
+                foreach($times as $k) {
+                    $open = $k->open;
+                    $distance = $k->km;
+                    echo "<li>Open:$open</li>";
+
+                }
+
+            }
+            ?>
+        </ul>
+        <h1>/ListClosedOnly</h1>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/ListClosedOnly');
+
+            $obj = json_decode($json);
+            $times = $obj->result;
+            if ($times == []){
+                echo "Database is empty"; 
+            } else{
+                foreach($times as $k) {
+                    $close = $k->close;
+                    $distance = $k->km;
+                    // not sure if I should print distances. 
+                    echo "<li>Close:$close</li>";
+
+                }
+
+            }
+            ?>
+        </ul>
     </body>
-</html>
+    </html>
+  
+
+
+
+
+       
